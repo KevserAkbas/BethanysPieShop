d697d26e56f1342039f62e762f1e49c537b6a858
KevserAkbas <45363875+KevserAkbas@users.noreply.github.com>
2021-05-18 00:11:38 +0300

    Controller Oluşturma #3
    * ASP.NET Core, belirli bir yerde controller ları arayacaktır ve bu Controllers klasörüdür, öyleyse önce onu oluşturalım.
    * bu ASP.NET Core MVC'nin kullandığı bir kuraldır.
    Orada controller larımız için arama yapacaktır.
    * Controller lara Controller ile biten bir ad vereceğiz ve ardından
    * ASP.NET Core bu controller ı temel olarak controller ı alarak ele alacaktır.
    * Bu, bir sınıftır.
    * Controller'da sona erer ve ayrıca ASP.NET Core MVC'den gelen temel Controller sınıfından miras alır.
    * PieController, varsayılan olarak ASP.NET Core ile birlikte gelen bağımlılık enjeksiyon sisteminden gerçekten yararlanacağımız ilk yerdir.
    * Burada depolarımıza (repositories) erişmemiz gerekecek.
    * Diyelim ki depolar aracılığıyla kontrol edilen verilere erişmemiz gerekecek.
    * Yani yapacağım şey, hem pasta hem de sahte depo için özel bir alan tanıtacağım ve bunun için arayüzleri kullanacağız.
    * Bunu yapıcıyı (constructor) kullanarak yapacağız.
    * Bu yüzden bir kurucu (constructor) getireceğim.
    * Ve bu kurucuya, şimdi bir IPieRepository ve bir ICategoryRepository geçeceğiz.
    * Şimdi, bunlar aslında controller ımıza enjekte edilecek çünkü onları startup da daha önce kaydetmiştik.
    * Bir sınıf bu türlerden herhangi birini gerektirdiğinde, yerleşik bağımlılık enjeksiyon sistemi tarafından otomatik olarak enjekte edilecektir.
    * Bu nedenle onları yenilememe gerek yok.
    * Elbette bunları takip etmemiz gerekiyor.
    * Yerel _pieRepository'imi enjekte edilecek olana ayarlayacağım ve tabii ki _categoryRepository için de aynı şeyi yapacağız.
    * Artık controllerdaki model sınıflarımıza erişimimiz var.
    * ViewResult, yine yerleşik bir ASP.NET Core MVC türüdür.
    * Ve bu yöntemi Liste olarak adlandıracağım çünkü çok yakında oluşturacağımız görünümde, uygulamadaki tüm pastaların bir listesini göstereceğiz.
    * bu noktada bir görünüm (view) döndürecek,
    * öyleyse ASP.NET Core'dan yerleşik bir yöntem olan View yöntemini geri getirelim ve bu yönteme bazı veriler aktaracağım.
    *bir controller oluşturmuş olsak da, henüz gerçekten hiçbir şey göremiyoruz.
    * Bir görünüm (View) eklememiz gerekecek.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        //Burada repository lerimize erişmemiz gerek
+
+        //private - özel
+        //readonly - salt okunur
+        private readonly IPieRepository _pieRepository; //1.bunlar sadece alandır, otomatik olarak başlatılmazlar,
+        private readonly ICategoryRepository _categoryRepository;//bunu constructor (yapıcı) kullanarak yaparız
+
+        public PieController(IPieRepository pieRepository, //2.bunlar controller ımıza enjekte edilecek
+            ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository; //3.artık controllerdaki model sınıflarımıza erişim var
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+
+    }
+}
